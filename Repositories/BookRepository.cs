089d687551248e1ed58c956a4fc28751bc84b6d1
spoage-commits <spoage@charlotte.edu>
2026-03-22 21:57:31 -0400

    Update BookRepository.cs

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -2,5 +2,20 @@ namespace DefaultNamespace;
 
 public class BookRepository
 {
-   
+   private List<Book> _books = new List<Book>();
+
+   public void Add(Book book) 
+   {
+      _books.Add(book);
+   }
+
+   public List<Book> GetAll() 
+   {
+      return _books;
+   }
+
+   public Book GetByISBN(string isbn) 
+   {
+      return _books.Find(b => b.ISBN == isbn);
+   }
 }
