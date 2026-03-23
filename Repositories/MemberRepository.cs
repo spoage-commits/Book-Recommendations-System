8c86bdd970b7f68e162dfda5be00f621ad2ab23d
spoage-commits <spoage@charlotte.edu>
2026-03-22 21:55:03 -0400

    Update MemberRepository.cs

--- Repositories/MemberRepository.cs
+++ Repositories/MemberRepository.cs
@@ -2,5 +2,21 @@ namespace DefaultNamespace;
 
 public class MemberRepository
 {
-    
+    private List<Member> _members = new List<Member>();
+
+    public void Add(Member member)
+    {
+        _members.Add(member);
+    }
+
+    public List<Members> GetAll() 
+    {
+        return _members; 
+    }
+
+    public Member GetById(int accountId)
+    {
+        return _members.Find(m => m.AccountId == accountId);
+    }
+        
 }
