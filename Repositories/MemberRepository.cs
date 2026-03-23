ba81e9fd090fde0968d9cce886941436e1e518fe
spoage-commits <spoage@charlotte.edu>
2026-03-22 21:04:53 -0400

    Update MemberRepository.cs
    Updated MemberRepo
    
    Now has add members, list of all members and search by member ID

--- Repositories/MemberRepository.cs
+++ Repositories/MemberRepository.cs
@@ -2,5 +2,24 @@ namespace DefaultNamespace;
 
 public class MemberRepository
 {
-    
+    //member list
+    private List<Member> _members = new List<Member>();
+
+    //adds member
+    public void Add(Member member)
+    {
+        _members.Add(member);
+    }
+
+    //returns all members
+    public List<Member> GetAll()
+    {
+        return _members;
+    }
+
+    //searches for members by matching account ID
+    public Member GetById(int accountId)
+    {
+        return _members.Find(m => m.AccountId == accountId);
+    }
 }
